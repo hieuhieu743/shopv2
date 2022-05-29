e0e1509b29576e74d781904ce121910f7f7a429b
hieuhieu743 <anhdaden50@gmail.com>
2022-05-29 16:18:59 +0700

    Updates

---
+++ precache-manifest.5135d95d5c6245bb6dbcbbd5550c5108.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "29dea7224179d66fdb21",
+    "url": "/shopv2/static/css/main.c293ae4e.chunk.css"
+  },
+  {
+    "revision": "29dea7224179d66fdb21",
+    "url": "/shopv2/static/js/main.53b10c67.chunk.js"
+  },
+  {
+    "revision": "27e9c6cae7d318c4d83f",
+    "url": "/shopv2/static/js/runtime~main.d3d044ab.js"
+  },
+  {
+    "revision": "1508b7613fca0c03f871",
+    "url": "/shopv2/static/js/2.33cc1c55.chunk.js"
+  },
+  {
+    "revision": "bb895545e484ff0164d4",
+    "url": "/shopv2/static/js/3.71a42320.chunk.js"
+  },
+  {
+    "revision": "6dd946ae7ed44b3d2d64ab84368980c6",
+    "url": "/shopv2/static/media/Roblox.6dd946ae.svg"
+  },
+  {
+    "revision": "037676ce4ec77ec23d73cf33e2d2612e",
+    "url": "/shopv2/static/media/Discord.037676ce.svg"
+  },
+  {
+    "revision": "17c98a043347ae04b958a0b7992e693f",
+    "url": "/shopv2/static/media/Twitter.17c98a04.svg"
+  },
+  {
+    "revision": "b0d84f4661cd51153d6e0e1cab667feb",
+    "url": "/shopv2/index.html"
+  }
+];
